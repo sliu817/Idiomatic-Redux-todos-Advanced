b22ed3f88247afe202e854684a921d57a6f49e3f
fograinwind <fograinwind@crunchbang.(none)>
2017-03-31 06:07:49 +0800

    L14-Fetching Data on Route change

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,20 +1,45 @@
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 import {withRouter} from 'react-router'
 import { getVisibleTodos} from '../reducers'
+import { fetchTodos } from '../api'
 
-const mapStateToProps = (state, {params}) => ({
-  todos: getVisibleTodos(state, params.filter || 'all')
-})
+class VisibleTodoList extends Component {
+  componentDidMount() {
+	 fetchTodos(this.props.filter).then(todos =>
+	   console.log(this.props.filter, todos));
+  }
+  
+  componentDidUpdate(prevProps) {
+	 if (this.props.filter !== prevProps.filter) {
+	   fetchTodos(this.props.filter).then(todos => 
+	     console.log(this.props.filter, todos));	 
+	 }  
+  }
+  
+  
+  render() {
+	return <TodoList {...this.props} />;
+   }	
+}
+
+const mapStateToProps = (state, {params}) => {
+  const filter = params.filter || 'all';
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  }
+}
 
 const mapDispatchToProps = {
   onTodoClick: toggleTodo
 }
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import configureStore from './configureStore';
 import Root from './components/Root'
-import {fetchTodos } from './api';
 
-
-fetchTodos('all').then(todos => console.log(todos));
 
 const store = configureStore();
 render(

7f65269105e5d5b470dbbad82855519d4cefb800
fograinwind <fograinwind@crunchbang.(none)>
2017-03-31 06:55:23 +0800

    L15-Dispatching Actions with the Fetched data

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
+import * as actions from '../actions'
 import TodoList from '../components/TodoList'
 import {withRouter} from 'react-router'
 import { getVisibleTodos} from '../reducers'
@@ -8,20 +8,25 @@ import { fetchTodos } from '../api'
 
 class VisibleTodoList extends Component {
   componentDidMount() {
-	 fetchTodos(this.props.filter).then(todos =>
-	   console.log(this.props.filter, todos));
+	 this.fetchData();
   }
   
   componentDidUpdate(prevProps) {
 	 if (this.props.filter !== prevProps.filter) {
-	   fetchTodos(this.props.filter).then(todos => 
-	     console.log(this.props.filter, todos));	 
+	   this.fetchData(); 
 	 }  
   }
   
+  fetchData() {
+	 const { filter, receiveTodos} = this.props;
+	 fetchTodos(filter).then(todos =>
+	   receiveTodos(filter,todos)
+	 ); 
+  }
   
   render() {
-	return <TodoList {...this.props} />;
+	const { toggleTodo, ...rest } = this.props;
+	return <TodoList {...rest} onTodoClick={toggleTodo} />;
    }	
 }
 
@@ -33,13 +38,9 @@ const mapStateToProps = (state, {params}) => {
   }
 }
 
-const mapDispatchToProps = {
-  onTodoClick: toggleTodo
-}
-
 VisibleTodoList = withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  actions
 )(VisibleTodoList));
 
 export default VisibleTodoList

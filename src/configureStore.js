5403a063f1d985dc163fe86453d7b999db4c5a79
fograinwind <fograinwind@crunchbang.(none)>
2017-03-31 05:05:05 +0800

    L12-Wrapping dispatch() to Log Actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,11 +4,30 @@ import reducer from './reducers'
 import { loadState, saveState } from './localStorage'
 import throttle from 'lodash/throttle';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+	return rawDispatch;  
+  }
+  
+  return (action) => {
+	console.group(action.type);
+	console.group('%c prev state', 'color:gray', store.getState());
+	console.log("%c action", 'color: blue', action);
+	const returnValue = rawDispatch(action);
+	console.group('%c next state', 'color: green', store.getState());
+	console.groupEnd(action.type);  
+	return returnValue;
+  };
+}
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(reducer, persistedState);
 
-  console.log(store.getState());
+  if (process.env.NODE_ENV !== 'production') {
+	store.dispatch = addLoggingToDispatch(store);  
+  }
 
   store.subscribe(throttle(()=> {
     saveState({todos: store.getState().todos});	
